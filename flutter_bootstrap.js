989241771e4c24b03d46aa9772c40dd00534cef9
heldermcosta <65597854+heldermcosta@users.noreply.github.com>
2026-02-19 23:14:50 +0000

    minor

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"d3d45dcf251823c1769909cd43698d126db38d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3688202114",
+    serviceWorkerVersion: "456186425",
   },
 });
