013a0b80f0f24d8cf89b90cc1e89ddfb44e95e35
heldermcosta <65597854+heldermcosta@users.noreply.github.com>
2026-02-20 13:16:07 +0000

    share button

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"d3d45dcf251823c1769909cd43698d126db38d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1289487163",
+    serviceWorkerVersion: "3025342014",
   },
 });
