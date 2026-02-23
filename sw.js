dbaaf52a748f0d120998fa233ddc11979c6fe4e4
heldermcosta <65597854+heldermcosta@users.noreply.github.com>
2026-02-23 22:03:03 +0000

    black screen inativity bug

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 ﻿'use strict';
 
 // Version changes on every build â€” forces old caches to be replaced.
-const CACHE = 'aw139pac-20260223214151';
+const CACHE = 'aw139pac-20260223220107';
 
 // Every file in the build is listed here so the service worker
 // can pre-cache all of them during install, enabling offline use
